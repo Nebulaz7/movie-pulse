ea43368cb017f3dde2bca97b7ad725fd4382f51b
sam <samuelmotari54@gmail.com>
2024-08-28 19:45:38 +0300

    initial

---
+++ front-end/src/components/Featured/Featured.jsx
@@ -0,0 +1,7 @@
+export function Featured(){
+    return (
+        <>
+        Featured
+        </>
+    )
+}
---
+++ front-end/src/components/Genre/Genre.jsx
@@ -0,0 +1,7 @@
+export function Genre(){
+    return (
+        <>
+        Genre
+        </>
+    )
+}
